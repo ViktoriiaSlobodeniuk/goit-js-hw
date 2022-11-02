ef8bd89e317a22c1744907083bf4d628c6d16571
ViktoriiaSlobodeniuk <110495710+ViktoriiaSlobodeniuk@users.noreply.github.com>
2022-11-03 00:36:33 +0200

    налаштування

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let base = prompt("give me number!");
+console.log(base);
+base = Number(base);
+console.log(typeof base);
+let power = prompt("give me another number, bitch!");
+console.log(power);
+// power = Number(power);
+console.log(typeof power);
+const result = base ** power;
+console.log(result);
+console.log(typeof result);
